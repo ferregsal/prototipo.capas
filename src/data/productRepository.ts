d6cc883c3155e51532815919d51c8723e5634965
ferregsal <fregidorsalazar@gmail.com>
2025-03-20 13:28:15 +0100

    Create Structure, Migrate, Add productRepository

---
+++ src/data/productRepository.ts
@@ -0,0 +1,46 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const ProductRepository = {
+    create: async (data: {
+        name: string;
+        price: number;
+        stock: number;
+        is_active: boolean;
+        created_at: Date;
+        Updated_at: Date;
+    }) => {
+        return prisma.product.create({ data });
+    },
+
+    //Encontrar todos los productos
+    findAll: async () => {
+        return prisma.product.findMany();
+    },
+
+    //Encontrar un producto por Id
+    findById: async (id: string) => {
+        return prisma.product.findUnique({ where: { id } });
+    },
+
+    // Actualizar un producto
+    update: async (
+        id: string,
+        data: {
+            name: string;
+            price: number;
+            stock: number;
+            is_active: boolean;
+            created_at: Date;
+            Updated_at: Date;
+        }
+    ) => {
+        return prisma.product.update({ where: { id }, data });
+    },
+
+    // Eliminar un producto
+    delete: async (id: string) => {
+        return prisma.product.delete({ where: { id } });
+    },
+};
